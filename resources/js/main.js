3e1df646ecd541c4f10ede9a05b2ac18d2994952
Luciano Brito <lucianobrito.dev@gmail.com>
2021-04-27 01:49:21 -0300

    add function clear fields and add loading.giff

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,35 +1,47 @@
-function findLyrics(artist, song) {
-    return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+function clearFields() {
+    let lyricsEl = document.querySelector("#lyrics");
+    let artist = document.querySelector("#artist").value.split(" ").join("-");
+    let song = document.querySelector("#song").value;
+    lyricsEl.innerHTML = "";
 }
 
-const form = document.querySelector('#lyrics-form');
+function findLyrics(artist, song) {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+}
 
-form.addEventListener('submit', el => {
-    el.preventDefault();
-    doSubmit();
-})
+const form = document.querySelector("#lyrics-form");
+
+form.addEventListener("submit", (el) => {
+  el.preventDefault();
+  doSubmit();
+});
 
 async function doSubmit() {
-    const lyricsEl = document.querySelector('#lyrics');
-    let artist = document.querySelector('#artist').value.split(" ").join("-");
-    let song = document.querySelector('#song').value;
+  let lyricsEl = document.querySelector("#lyrics");
+  let artist = document.querySelector("#artist").value.split(" ").join("-");
+  let song = document.querySelector("#song").value;
 
-    if (artist == '' || song == '') {
-        return alert('Os campos não podem estar vazios!\n\nPor favor, digite o nome do artista e a música para realizarmos a busca.')
+  if (artist == "" || song == "") {
+    return alert(
+      "Os campos não podem estar vazios!\n\nPor favor, digite o nome do artista e a música para realizarmos a busca."
+    );
+  }
+  clearFields();
+  document.getElementById("loading").style.display = "block";
+
+  //async await
+  try {
+    const lyricsResponse = await findLyrics(artist, song);
+    const data = await lyricsResponse.json();
+    document.getElementById("loading").style.display = "none";
+    if (data.lyrics && data) {
+      lyricsEl.innerHTML = data.lyrics;
+    } else {
+      lyricsEl.innerHTML = data.error;
     }
-
-    lyricsEl.innerHTML = '<div class="spinner-border text-darck" role="status"><span class="sr-only">Loading...</span></div>';
-
-    //async await
-    try {
-        const lyricsResponse = await findLyrics(artist, song);
-        const data = await lyricsResponse.json();
-        if(data.lyrics && data) {
-            lyricsEl.innerHTML = data.lyrics;
-        } else {
-            lyricsEl.innerHTML = data.error;
-        }
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+/* <div class="clearfix"><div class="spinner-border float-end" role="status"><span class="visually-hidden"></span></div></div> */
