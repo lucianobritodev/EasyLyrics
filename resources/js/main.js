0c4ea47c27c9b66c888ba643f66b134e67643127
lucianobritodev <lucianobrito.dev@gmail.com>
2021-11-03 13:24:17 -0300

    Added controls methods for apikeys

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,30 @@
 const form = document.querySelector("#lyrics-form");
 
+async function readKeys() {
+  try {
+    let data = await fetch("../../config.json")
+    return data.json();
+  } catch (e) {
+    console.log(e.message);
+    return null;
+  }
+}
+
 form.addEventListener("submit", (el) => {
   el.preventDefault();
   doSubmit();
 });
 
 async function doSubmit() {
+  const vagalumeUrl = document.querySelector("#vagalumeUrl");
+  const artistAndSong = document.querySelector("#artistAndSong");
+  const keys = await readKeys();
+
   let lyricsEl = document.querySelector("#lyrics");
   let artistRaw = document.querySelector("#artist").value;
   let songRaw = document.querySelector("#song").value;
   let artist = artistRaw.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(' ').join('-');
   let song = songRaw.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(' ').join('-');
-  const key = '0baa17731bcd852efce9c8c9753e13f2';
-  const vagalumeUrl = document.querySelector("#vagalumeUrl");
-  const artistAndSong = document.querySelector("#artistAndSong");
 
   if (artist == '' || song == '') {
     return Swal.fire('Ops!', 'Algo deu errado. Por favor, preencha os campos corretamente!', 'error')
@@ -23,7 +34,11 @@ async function doSubmit() {
   document.querySelector("#loading").style.display = "block";
 
   try {
-    const [ vagalumeJson, lyricsJson ] = await findLyrics(artist, song, key);
+    const [ vagalumeJson, lyricsJson ] = await findLyrics(artist, song, keys);
+
+    console.log("\nVagalume: \n" + vagalumeJson);
+   // console.log("\nMusixmatch: \n" + musixmatchJson);
+    console.log("\nLyrics: \n" + lyricsJson);
     
     document.querySelector("#loading").style.display = "none";
     
@@ -37,12 +52,12 @@ async function doSubmit() {
         lyricsEl.innerHTML = lyricsJson.lyrics;
       }
     } else {
-      throw new Error('No lyrics found!')
+      throw new Error('Canção não encontrada!')
     }
 
   } catch (error) {
     document.querySelector("#loading").style.display = "none";
-    lyricsEl.innerHTML = error.message;
+    lyricsEl.innerHTML = "Ops! Algo deu errado. \n" + error.message;
   }
 }
 
@@ -51,16 +66,32 @@ function clearFields() {
   document.querySelector("#vagalumeContainer").style.display = "none";
 }
 
-async function findLyrics(artist, song, key) {
+async function findLyrics(artist, song, keys) {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "text/plain");
+
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+  };
+  
+  let vagalumeRequest = new Request(`https://api.vagalume.com.br/search.php?art=${artist}&mus=${song}&limit=1&apikey=${keys[0].vagalumeApiKey}`, myInit);
+  //let musixmatchRequest = new Request(`https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_artist=${artist}&q_track=${song}&apikey=${keys[1].musixmatchApiKey}&format=json`, myInit);
+  let lyricsRequest = new Request(`https://api.lyrics.ovh/v1/${artist}/${song}`, myInit);
+
   const [ responseVagalume, responseLyrics ] = await Promise.all([
-    fetch(`https://api.vagalume.com.br/search.php?art=${artist}&mus=${song}&limit=1&apikey=${key}`),
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    fetch(vagalumeRequest),
+    //fetch(musixmatchRequest),
+    fetch(lyricsRequest)
   ]);
 
   const vagalumeJson = await responseVagalume.json();
+  // const musixmatchJson = await responseMusixmatch.json(); responseMusixmatch,
   const lyricsJson = await responseLyrics.json();
 
-  return [ vagalumeJson, lyricsJson ];
+  return [ vagalumeJson, lyricsJson ]; // musixmatchJson,
 }
 
 function titleCase(str) {
@@ -69,4 +100,3 @@ function titleCase(str) {
   })
   return title;
 }
-/* <div class="clearfix"><div class="spinner-border float-end" role="status"><span class="visually-hidden"></span></div></div> */
